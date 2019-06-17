fa08b4ce787732a94a7ee1d3e2289926a57be954
Johannes Rudolph <johannes.rudolph@gmail.com>
2019-06-17 16:50:49 +0200

    changed to new crossover version

--- src/test/scala/net/virtualvoid/genie/tsp/TSP.scala
+++ src/test/scala/net/virtualvoid/genie/tsp/TSP.scala
@@ -17,7 +17,21 @@ trait TSP {
   def numCities: Int = cities.size
   def cities: Vector[City]
 
-  case class Path(vertexIndices: Vector[Int]) {
+  case class Path(indices: Vector[Int]) {
+    //require(indices.toSet.size == numCities, s"Path should have $numCities distinct parts but had only ${indices.toSet.size}: ${indices.mkString(", ")}")
+
+    def pathCities: Seq[City] = indices.map(cities)
+    lazy val length: Double = {
+      val cities = pathCities
+      val fst = cities.head
+      cities.tail.foldLeft((fst, 0d)) { (state, nextCity) =>
+        val newDist = state._2 + distance(state._1, nextCity)
+        (nextCity, newDist)
+      }._2
+    }
+  }
+
+  case class Path2(vertexIndices: Vector[Int]) {
     lazy val pathCities: Seq[City] = {
       @tailrec def rec(remainingPath: Vector[Int], remainingCities: Vector[City], reversePath: List[City]): Seq[City] =
         if (remainingPath.isEmpty) {
@@ -42,13 +56,56 @@ trait TSP {
       }._2
     }
   }
-  implicit val intRanges: Vector[IntRange[Int]] = IntRange(0, 1, identity) +: ((numCities - 1) to 1 by -1).map(num => IntRange(0, num, identity)).toVector
+  //implicit val intRanges: Vector[IntRange[Int]] = IntRange(0, 1, identity) +: ((numCities - 1) to 1 by -1).map(num => IntRange(0, num, identity)).toVector
   implicit def samplePath: Sample[Path] =
     Sample { random =>
-      Path(intRanges.map(_.sample(random)))
+      def select(remainingCities: Seq[Int], currentPath: List[Int]): Path =
+        if (remainingCities.isEmpty) Path(currentPath.toVector)
+        else {
+          val selected = random.nextInt(remainingCities.size)
+          val remaining = remainingCities.take(selected) ++ remainingCities.drop(selected + 1)
+          select(remaining, remainingCities(selected) :: currentPath)
+        }
+
+      select((1 until numCities), 0 :: Nil)
+      //Path(intRanges.map(_.sample(random)))
     }
 
-  implicit val crossoverPath: Crossover[Path] =
+  implicit val noPathCrossover: Crossover[Path] = Crossover.disable[Path]
+  val crossoverPath: Crossover[Path] =
+    Crossover { (path1, path2, random) =>
+      val pivot1 = random.nextInt(numCities)
+      val pivot2 = numCities - pivot1
+
+      val new1Head = path1.indices.take(pivot1)
+      val new1Remaining =
+        path1.indices.drop(pivot1).toSet
+      val new1Tail =
+        path1.indices.filter(new1Remaining)
+
+      val new2Tail = path2.indices.takeRight(pivot2)
+      val new2Remaining = path2.indices.dropRight(pivot2).toSet
+      val new2Head = path2.indices.filter(new2Remaining)
+
+      (Path(new1Head ++ new1Tail), Path(new2Head ++ new2Tail))
+    }
+
+  def perPathMutationRate: Double = 1
+  implicit val mutatePath: Mutate[Path] =
+    Mutate { (path, random) =>
+      if (random.nextDouble() < perPathMutationRate) {
+        val indicesArray = path.indices.toArray
+        val a = random.nextInt(numCities)
+        val b = random.nextInt(numCities)
+        val s = indicesArray(a)
+        indicesArray(a) = indicesArray(b)
+        indicesArray(b) = s
+
+        Path(indicesArray.toVector)
+      } else path
+    }
+
+  /*implicit val crossoverPath: Crossover[Path] =
     Crossover { (path1, path2, random) =>
       val pivot = random.nextInt(numCities)
 
@@ -69,5 +126,5 @@ trait TSP {
             i
       }
       Path(indices)
-    }
+    }*/
 }
--- src/test/scala/net/virtualvoid/genie/tsp/TspTest.scala
+++ src/test/scala/net/virtualvoid/genie/tsp/TspTest.scala
@@ -1,21 +1,26 @@
 package net.virtualvoid.genie.tsp
 
+import java.io.{ File, FileOutputStream }
 import java.nio.file.Paths
 
 import net.virtualvoid.genie.{ Crossover, Mutate, Sample }
 
 import scala.annotation.tailrec
+import scala.io.Source
 import scala.util.Random
 
 object TspTest extends App with TSP with TspFileLoader with TspPlotter {
-  val populationSize = 200
-  val topN = 2
-  val randomNewPerGeneration = 10
+  val populationSize = 2000
+  val topN = 50
+  val randomNewPerGeneration = 0
   val maxGenerations = 100
 
   lazy val cities: Vector[TspTest.City] = load(Paths.get("/home/maike/tmp/repos/genie/src/test/resources/qa194.tsp"))
   println(s"Found $numCities cities")
-  val inOrder = Path(Vector.fill(numCities)(0))
+  val inOrder = Path((0 until numCities).toVector)
+  val inOrderY = Path {
+    cities.zipWithIndex.sortBy(_._1.position._2).map(_._2)
+  }
   //inOrder.pathCities.foreach(println)
   println(s"Length when visiting in order: ${inOrder.length}")
   plotPath(inOrder, "inOrder.png")
@@ -25,12 +30,33 @@ object TspTest extends App with TSP with TspFileLoader with TspPlotter {
   def sample(num: Int, random: Random): Vector[Path] =
     Vector.fill(num)(Sample.create[Path](random))
 
+  def savePath(path: Path): Unit = {
+    val target = new File(s"best/${path.length}-${path.hashCode()}.txt")
+    if (!target.exists()) {
+      val fos = new FileOutputStream(target)
+      path.indices.foreach { i =>
+        fos.write(i.toString.getBytes("utf8"))
+        fos.write('\n')
+      }
+      fos.close()
+    }
+  }
+  def loadPath(file: File): Path =
+    Path(Source.fromFile(file).getLines.map(_.toInt).toVector)
+  def loadPaths(): Seq[Path] = {
+    val folder = new File("best")
+    folder.listFiles()
+      .filter(_.getName endsWith ".txt")
+      .map(loadPath)
+  }
+
   @tailrec
   def step(generation: Int, lastBest: Double, population: Vector[Path]): Unit = {
     population.par.foreach(_.length)
     val sorted = population.sortBy(_.length)
     println(f"[$generation%5d] best: ${sorted.head.length}%7.0f worst: ${sorted.last.length}%7.0f improvement: ${(lastBest / sorted.head.length - 1) * 100}%5.2f %%")
     plotPath(sorted.head, f"generational/gen$generation%05d.png")
+    savePath(sorted.head)
     if (generation < maxGenerations /*&& sorted.head.length < lastBest*/ ) {
       val top = population.sortBy(_.length).take(topN)
 
@@ -58,5 +84,13 @@ object TspTest extends App with TSP with TspFileLoader with TspPlotter {
       plotPath(best, "best.png")
     }
   }
-  step(0, Double.MaxValue, sample(populationSize, rand) /*:+ inOrder*/ )
+  step(0, Double.MaxValue, sample(populationSize, rand) ++ loadPaths() :+ inOrder :+ inOrderY)
+
+  /*val Seq(p1, p2) = sample(2, rand)
+  val (n1, n2) = Crossover.cross[Path](p1, p2, rand)
+
+  println(p1.indices.mkString(", "))
+  println(p2.indices.mkString(", "))
+  println(n1.indices.mkString(", "))
+  println(n2.indices.mkString(", "))*/
 }
